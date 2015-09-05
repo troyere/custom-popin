9bb94ad5b73cc92d4f9505a902c2de527547bc4e
Tanguy ROYERE <tanguy.royere@gmail.com>
2015-09-05 13:54:14 +0200

    Better form validation

--- src/AppBundle/Services/ConfigValidatorService.php
+++ src/AppBundle/Services/ConfigValidatorService.php
@@ -13,15 +13,21 @@ class ConfigValidatorService
      */
     public function validate(array $config)
     {
+        $sizeMode = isset($config['sizeMode']) ? $config['sizeMode'] : null;
+        $width    = isset($config['width']) ? $config['width'] : null;
+        $height   = isset($config['height']) ? $config['height'] : null;
+
         $errors = array();
-        if (isset($config['sizeMode']) && $config['sizeMode'] === 'custom' &&
-            (!isset($config['width']) || isset($config['height']))) {
+        if ($sizeMode === 'custom' && (empty($width) || empty($height))) {
             $errors[] = 'Width and height are both required if the size mode equals "custom".';
         }
-        if (isset($config['width']) && !is_numeric($config['width'])) {
+        if ($sizeMode !== 'custom' && (!empty($width) || !empty($height))) {
+            $errors[] = 'Width and height are allowed only if the size mode equals "custom".';
+        }
+        if (!empty($width) && !is_numeric($width)) {
             $errors[] = 'Width must be an integer.';
         }
-        if (isset($config['height']) && !is_numeric($config['height'])) {
+        if (!empty($height) && !is_numeric($height)) {
             $errors[] = 'Height must be an integer.';
         }
         return $errors;
